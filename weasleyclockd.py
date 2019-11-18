13f236b93d4792d3b019c5d14e359db4af0a6961
Bryn Dole <bryn.dole@gmail.com>
2019-11-17 20:54:34 -0800

    rough sketch. no testing

--- weasleyclockd.py
+++ weasleyclockd.py
@@ -6,35 +6,85 @@ import argparse
 import logging
 import daemon
 import json
+import paho.mqtt.client as mqtt
 from daemon import pidfile
 
-debug_p = False
+debug_p = True
 
 
-def do_something(logf):
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    userdata.logger.INFO("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(userdata['topic')
+
+
+def on_message(client, userdata, message):
+    userdata.logger.INFO("Received message '" + str(message.payload) +
+                         "' on topic '" + message.topic +
+                         "' with QoS " + str(message.qos))
+
+
+def do_something(logf, configf):
     # This does the "work" of the daemon
 
+    #
+    # setup logging
+    #
     logger = logging.getLogger('eg_daemon')
     logger.setLevel(logging.INFO)
-
     fh = logging.FileHandler(logf)
     fh.setLevel(logging.INFO)
-
     formatstr = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     formatter = logging.Formatter(formatstr)
-
     fh.setFormatter(formatter)
-
     logger.addHandler(fh)
 
-    while True:
-        logger.debug("this is a DEBUG message")
-        logger.info("this is an INFO message")
-        logger.error("this is an ERROR message")
-        time.sleep(5)
+    # read config file
+    with open(configf) as json_data_file:
+        config_data = json.load(json_data_file)
+
+    # connect to MQTT server
+    host = config_data['mqtt_host']
+    port = config_data['mqtt_port'] if 'mqtt_port' in config_data else 4884
+    topic = config_data['mqtt_topic'] if 'mqtt_topic' in config_data else 'weasleyclock/#'
+
+    logger.info("Weasley Clock: connecting to host " + host + ":" + port +
+                " topic " + topic)
+
+    clockdata = {
+        'logger': logger,
+        'host': host,
+        'port': port,
+        'topc': topic,
+        'config_data': config_data,
+        }
+
+    mqttc = mqtt.Client(client_id='',
+                        clean_session=True,
+                        userdata=clockdata)
+    mqttc.subscribe(topic)
+
+    # create callbacks
+    # https://pypi.org/project/paho-mqtt/
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+
+    # intitialize clock hands
+
+    mqttc.connect(host, port, 60)
+
+    # loop
+    mqttc.loop_forever()
+
+#    while True:
+#        logger.info("this is an INFO message")
+#        time.sleep(5)
 
 
-def start_daemon(pidf, logf):
+def start_daemon(pidf, logf, configf):
     # This launches the daemon in its context
 
     global debug_p
@@ -50,7 +100,7 @@ def start_daemon(pidf, logf):
         umask=0o002,
         pidfile=pidfile.TimeoutPIDLockFile(pidf),
         ) as context:
-        do_something(logf)
+        do_something(logf, configf)
 
 
 if __name__ == "__main__":
@@ -61,4 +111,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    start_daemon(pidf=args.pid_file, logf=args.log_file)
+    start_daemon(pidf=args.pid_file, logf=args.log_file, configf=args.config_file)
