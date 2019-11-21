75078cd0c88216ccbba60a7cb25566d9ff68cc26
Bryn Dole <bryn.dole@gmail.com>
2019-11-21 10:40:39 -0800

    physical clock layout. less logging

--- weasleyclockd.py
+++ weasleyclockd.py
@@ -12,14 +12,89 @@ from geopy.distance import great_circle
 
 debug_p = True
 
+#
+# Configuration of the Clock face
+#   name:    pretty name
+#   angle:   starting angle of the clock sector (degrees)
+#   theta:   width of clock sector in degrees
+#   offset_style:   type of sub-sector hand placement
+#
+states = {
+    'home': {
+        'name': 'Home',
+        'angle': 0,
+        'theta': 22.5,
+        'offset_style': 'home'
+        },
+    'barn': {
+        'name': 'Barn',
+        'angle': 22.5,
+        'theta': 22.5,
+        'offset_style': 'home'
+        },
+    'mortalperil': {
+        'name': 'Mortal Peril',
+        'angle': 45,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'quidditch': {
+        'name': 'Quidditch',
+        'angle': 90,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'work': {
+        'name': 'Work',
+        'angle': 135,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'school': {
+        'name': 'School',
+        'angle': 180,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'garden': {
+        'name': 'Garden',
+        'angle': 225,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'intransit': {
+        'name': 'In Transit',
+        'angle': 270,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'lost': {
+        'name': 'Lost',
+        'angle': 315,
+        'theta': 45,
+        'offset_style': 'distance'
+        },
+    'error': {
+        'name': 'Error',
+        'angle': 0,
+        'theta': 0,
+        'offset_style': 'distance'
+        },
+    }
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     client.subscribe(userdata['topic'])
-    userdata['logger'].info("subscibing to topic [" + userdata['topic'] +
-                            "] result code " + str(rc))
+    if rc != 0:
+        userdata['logger'].info("subscibing to topic [" + userdata['topic'] +
+                                "] result code " + str(rc))
+    else:
+        userdata['logger'].debug("subscibing to topic [" + userdata['topic'] +
+                                 "] result code " + str(rc))
 
 
+# Callback for MQTT messages
 def on_message(client, userdata, message):
     # wrap the on_message() processing in a try:
     try:
@@ -41,8 +116,8 @@ def _on_message(client, userdata, message):
     log_snippet = log_snippet.replace('\n', ' ')
 
     (prefix, name) = topic.split('/', 1)
-    
-    userdata['logger'].info("Received message '" +
+
+    userdata['logger'].debug("Received message '" +
                             log_snippet +
                             "' on topic '" + topic +
                             "' with QoS " + str(message.qos))
@@ -81,9 +156,15 @@ def move_clock_hands(name, message, userdata):
     print("Move " + name + " hand to " + state +
           " ({0:.1f} miles away)".format(distance))
 
+    # hand    = config_data.hand[name]
+    # channel = config_data.channel[hand]
+    
     userdata['logger'].info("Move [" + name +
                             "] hand to [" + state +
                             "] ({0:.1f} miles away)".format(distance))
+
+
+
 
 def do_something(logf, configf):
 
