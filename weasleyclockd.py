8d4dd960420d83e1fb946070ebf505437a5c1947
Bryn Dole <bryn.dole@gmail.com>
2020-10-06 13:00:45 -0700

    big reorg. add asyncio

--- weasleyclockd.py
+++ weasleyclockd.py
@@ -9,6 +9,7 @@ import json
 import paho.mqtt.client as mqtt
 import lockfile
 import numpy as np
+import asyncio
 from geopy.distance import great_circle
 from adafruit_servokit import ServoKit
 
@@ -28,13 +29,15 @@ states = {
         'name': 'Home',
         'angle': 340,
         'theta': 20,
-        'offset_style': 'staggered'
+        'offset_style': 'staggered',
+        'update_delay': 30
         },
     'barn': {
         'name': 'Barn',
         'angle': 320,
         'theta': 20,
-        'offset_style': 'staggered'
+        'offset_style': 'staggered',
+        'update_delay': 30
         },
     'mortalperil': {
         'name': 'Mortal Peril',
@@ -58,13 +61,15 @@ states = {
         'name': 'School',
         'angle': 140,
         'theta': 40,
-        'offset_style': 'staggered'
+        'offset_style': 'staggered',
+        'update_delay': 60,
         },
     'garden': {
         'name': 'Garden',
         'angle': 90,
         'theta': 50,
-        'offset_style': 'staggered'
+        'offset_style': 'staggered',
+        'update_delay': 30
         },
     'intransit': {
         'name': 'In Transit',
@@ -128,7 +133,7 @@ def log_distance(distance):
     return scale
 
 
-def angle_offset(angle, theta, distance, hand, style):
+def angle_offset(state, angle, theta, distance, hand, style):
     '''
     Calculate where in the sector to point the clock hands.
     '''
@@ -142,40 +147,44 @@ def angle_offset(angle, theta, distance, hand, style):
     elif style == 'staggered' or style == 'home':
         # each hand 0-3 has it's own small offset
         # TODO: redo this to take into account how many hands are in this sector
-        scale = 0.8 * (float(hand) / 3.0) + 0.1
+        num_hands = num_hands_in_state(state)
+        if num_hands < 2:
+            scale = 0.5
+        else:
+            scale = 0.8 * (float(hand % num_hands) / float(num_hands + 1)) + 0.1
         return scale * theta
     # middle of the sector
     return (theta / 2.0)
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, clockdata, flags, rc):
     '''
     The callback for when the client receives a CONNACK response from the server.
     '''
-    client.subscribe(userdata['topic'])
+    client.subscribe(clockdata['topic'])
     client.publish('weasleyclock/UPDATE', payload='{"update":"true"}', qos=0, retain=False)
     if rc != 0:
-        userdata['logger'].warning("subscibing to topic [" +
-                                   userdata['topic'] +
-                                   "] result code " + str(rc))
+        clockdata['logger'].warning("subscibing to topic [" +
+                                    clockdata['topic'] +
+                                    "] result code " + str(rc))
     else:
-        userdata['logger'].debug("subscibing to topic [" +
-                                 userdata['topic'] +
-                                 "] result code " + str(rc))
+        clockdata['logger'].debug("subscibing to topic [" +
+                                  clockdata['topic'] +
+                                  "] result code " + str(rc))
 
 
-def on_message(client, userdata, message):
+def on_message(client, clockdata, message):
     '''
     Callback for recieved MQTT messages.
     '''
     # wrap the on_message() processing in a try:
     try:
-        _on_message(client, userdata, message)
+        _on_message(client, clockdata, message)
     except Exception as e:
-        userdata['logger'].error("on_message() failed: {}".format(e))
+        clockdata['logger'].error("on_message() failed: {}".format(e))
 
 
-def _on_message(client, userdata, message):
+def _on_message(client, clockdata, message):
     '''
     Callback for recieved MQTT messages.
     '''
@@ -196,10 +205,10 @@ def _on_message(client, userdata, message):
     log_snippet = (m_decode[:15] + '..') if len(m_decode) > 17 else m_decode
     log_snippet = log_snippet.replace('\n', ' ')
 
-    userdata['logger'].debug("Received message '" +
-                             log_snippet +
-                             "' on topic '" + topic +
-                             "' with QoS " + str(message.qos))
+    clockdata['logger'].debug("Received message '" +
+                              log_snippet +
+                              "' on topic '" + topic +
+                              "' with QoS " + str(message.qos))
 
     try:
         msg_data = json.loads(m_decode)
@@ -208,26 +217,23 @@ def _on_message(client, userdata, message):
             print("JSON decode failed. [" + parse_error.msg + "]")
             print("error at pos: " + parse_error.pos +
                   " line: " + parse_error.lineno)
-        userdata['logger'].error("JSON decode failed.")
+        clockdata['logger'].error("JSON decode failed.")
 
     # python <=3.4.* use ValueError
     # except ValueError as parse_error:
     #    if debug_p:
     #        print("JSON decode failed: " + str(parse_error))
 
-    move_clock_hands(name, msg_data, userdata)
+    update_clock_state(name, msg_data, clockdata)
 
 
-def update_hand_state(name, state, style, distance, offset, servo_angle):
+def update_hand_state(name, state, style, distance):
     '''
     Set the clock hand position in the global state.
     '''
     current_state[name] = {
         'state': state,
-        'syle': style,
         'distance': distance,
-        'offset': offset,
-        'servo_angle': servo_angle,
         'updated': time.time()
         }
 
@@ -242,16 +248,96 @@ def num_hands_in_state(state):
             count = count + 1
     return count
 
-# TODO: function to update all the clock hand positions
-# TODO: only update hand position for Garden/Home/Barn if it's been the same for 15 seconds (configurable)
+
+def update_all_hands(clockdata):
+    # TODO: function to update all the clock hand positions
+    '''
+    Iterate over all the hands and update the current physical postion to match the current state.
+    '''
+    now = time.time()
+    config_data = clockdata['config_data']
+    delayed_update = False
+    for name in current_state:
+        state = current_state['state']
+        if 'update_delay' in config_data[state]:
+            update_delay = config_data[state]['update_delay']
+        else:
+            update_delay = 0
+        if current_state[name].updated + update_delay <= now:
+            move_clock_hand(current_state[name], clockdata)
+        else:
+            delayed_update = True
+            if debug_p:
+                print("delay update of [" + name + "] to state [" + state + "] for " + update_delay)
+
+    if delayed_update:
+        await asyncio.sleep(1)
+        if debug_p:
+            print("retrying update_all_hands")
+        update_all_hands(clockdata)
+
+
+def move_clock_hand(userstate, clockdata):
+    '''
+    Pysically move a single hand given its state.
+    '''
+    name = userstate['name']
+    state = userstate['state']
+    distance = userstate['distance']
+    config_data = clockdata['config_data']
+    if debug_p:
+        print("Move " + name + " hand to " + state +
+              " ({0:.1f} miles away)".format(distance))
+
+    if name not in config_data['hand']:
+        if debug_p:
+            print("Person " + name + " is not tracked by a clock hand")
+        return
+    hand = config_data['hand'][name]
+
+    if hand not in config_data['channel']:
+        clockdata['logger'].error("Hand " + hand +
+                                  " does not have a specified PWM channel")
+        return
+    channel = config_data['channel'][hand]
+
+    if state not in states:
+        clockdata['logger'].error("Unknown target state [" + state + "]")
+        return
+
+    target_state = states[state]
+    base_angle = float(target_state['angle'])
+    theta = float(target_state['theta'])
+    style = target_state['offset_style']
+
+    # Use hand style for Quidditch when still at home. This is to cover
+    # the case when riding Zwift indoors.
+    if state == 'quidditch' and distance < 0.2:
+        style = 'staggered'
+
+    offset = angle_offset(state, base_angle, theta, distance, hand, style)
+    # add 720 to keep servos closer to the center of their range
+    servo_angle = int(2 * (base_angle + offset)) + 720
+    clockdata['kit'].servo[channel].angle = servo_angle
+
+    clockdata['logger'].info("Move [" + name +
+                             "] hand to [" + state +
+                             "] ({0:.1f} miles away)".format(distance))
+
+    if debug_p:
+        print("base_angle [" + str(base_angle) + "] theta [" + str(theta) +
+              "] style [" + style + "]")
+        print("distance [", distance, "]  offset [", offset, "]")
+        print("servo angle: ", servo_angle)
+
+
 # TODO: call update hand function periodically (use scheduler)
 
-
-def move_clock_hands(name, message, userdata):
+def update_clock_state(name, message, clockdata):
     '''
-    Move Clock Hands. Move user hand to the indicated state and style.
+    Parse message data and set the person's clock hand state. Postion is updated seprately.
     '''
-    config_data = userdata['config_data']
+    config_data = clockdata['config_data']
     state = None
     latitude = None
     longitude = None
@@ -272,52 +358,8 @@ def move_clock_hands(name, message, userdata):
         distance = great_circle((latitude_home, longitude_home),
                                 (latitude, longitude)).miles
 
-    if debug_p:
-        print("Move " + name + " hand to " + state +
-              " ({0:.1f} miles away)".format(distance))
-
-    if name not in config_data['hand']:
-        if debug_p:
-            print("Person " + name + " is not tracked by a clock hand")
-        return
-    hand = config_data['hand'][name]
-
-    if hand not in config_data['channel']:
-        userdata['logger'].error("Hand " + hand +
-                                 " does not have a specified PWM channel")
-        return
-    channel = config_data['channel'][hand]
-
-    if state not in states:
-        userdata['logger'].error("Unknown target state [" + state + "]")
-        return
-
-    target_state = states[state]
-    base_angle = float(target_state['angle'])
-    theta = float(target_state['theta'])
-    style = target_state['offset_style']
-
-    # Use hand style for Quidditch when still at home. This is to cover
-    # the case when riding Zwift indoors.
-    if state == 'quidditch' and distance < 0.2:
-        style = 'staggered'
-
-    offset = angle_offset(base_angle, theta, distance, hand, style)
-    # add 720 to keep servos closer to the center of their range
-    servo_angle = int(2 * (base_angle + offset)) + 720
-    userdata['kit'].servo[channel].angle = servo_angle
-
-    update_hand_state(name, state, style, distance, offset, servo_angle)
-
-    userdata['logger'].info("Move [" + name +
-                            "] hand to [" + state +
-                            "] ({0:.1f} miles away)".format(distance))
-
-    if debug_p:
-        print("base_angle [" + str(base_angle) + "] theta [" + str(theta) +
-              "] style [" + style + "]")
-        print("distance [", distance, "]  offset [", offset, "]")
-        print("servo angle: ", servo_angle)
+    update_hand_state(name, state, distance)
+    update_all_hands(clockdata)
 
 
 def do_something(logf, configf):
@@ -392,7 +434,7 @@ def do_something(logf, configf):
         kit.servo[servo].actuation_range = actuation_range
         kit.servo[servo].set_pulse_width_range(pulsewidth_min, pulsewidth_max)
 
-    clockdata = {
+    m_clockdata = {
         'logger': logger,
         'host': host,
         'port': port,
@@ -404,7 +446,7 @@ def do_something(logf, configf):
     # how to mqtt in python see https://pypi.org/project/paho-mqtt/
     mqttc = mqtt.Client(client_id='weasleyclockd',
                         clean_session=True,
-                        userdata=clockdata)
+                        clockdata=m_clockdata)
 
     mqttc.username_pw_set(config_data['mqtt_user'],
                           config_data['mqtt_password'])
